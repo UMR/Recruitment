8c1e15f34500f34febd602bee52478c26a029736
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-05-19 15:36:26 +0600

    add home controller

---
+++ Services/Recruitment/Recruitment.API/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Recruitment.API.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Welcome to Recruitment Resource Server");
+        }
+    }
+}
