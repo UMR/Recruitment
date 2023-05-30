f0a809eb2a3a136cd3000887e214b549316103ba
Sohel Rana <srana@ael-bd.com>
2023-05-30 18:34:30 +0600

    Update app-routing.module.ts

--- Recruitment.Web/src/app/app-routing.module.ts
+++ Recruitment.Web/src/app/app-routing.module.ts
@@ -5,67 +5,67 @@ import { DefaultLayoutComponent } from './containers';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full'
-  },
-  {
-    path: '',
-    component: DefaultLayoutComponent,
-    data: {
-      title: 'Home'
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
     },
-    children: [
-      {
-        path: 'dashboard',
-        loadChildren: () =>
-          import('./dashboard/dashboard.module').then((m) => m.DashboardModule)
-      },
-      
-    ]
-  },
-  //{
-  //  path: '404',
-  //  component: Page404Component,
-  //  data: {
-  //    title: 'Page 404'
-  //  }
-  //},
-  //{
-  //  path: '500',
-  //  component: Page500Component,
-  //  data: {
-  //    title: 'Page 500'
-  //  }
-  //},
-  {
-    path: 'login',
-    component: LoginComponent,
-    data: {
-      title: 'Login Page'
-    }
-  },
-  //{
-  //  path: 'register',
-  //  component: RegisterComponent,
-  //  data: {
-  //    title: 'Register Page'
-  //  }
-  //},
-  {path: '**', redirectTo: 'dashboard'}
+    {
+        path: '',
+        component: DefaultLayoutComponent,
+        data: {
+            title: 'Home'
+        },
+        children: [
+            {
+                path: 'dashboard',
+                loadChildren: () =>
+                    import('./dashboard/dashboard.module').then((m) => m.DashboardModule)
+            },
+
+        ]
+    },
+    //{
+    //  path: '404',
+    //  component: Page404Component,
+    //  data: {
+    //    title: 'Page 404'
+    //  }
+    //},
+    //{
+    //  path: '500',
+    //  component: Page500Component,
+    //  data: {
+    //    title: 'Page 500'
+    //  }
+    //},
+    {
+        path: 'login',
+        component: LoginComponent,
+        data: {
+            title: 'Login Page'
+        }
+    },
+    //{
+    //  path: 'register',
+    //  component: RegisterComponent,
+    //  data: {
+    //    title: 'Register Page'
+    //  }
+    //},
+    { path: '**', redirectTo: 'dashboard' }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, {
-      scrollPositionRestoration: 'top',
-      anchorScrolling: 'enabled',
-      initialNavigation: 'enabledBlocking'
-      // relativeLinkResolution: 'legacy'
-    })
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, {
+            scrollPositionRestoration: 'top',
+            anchorScrolling: 'enabled',
+            initialNavigation: 'enabledBlocking'
+            // relativeLinkResolution: 'legacy'
+        })
+    ],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
