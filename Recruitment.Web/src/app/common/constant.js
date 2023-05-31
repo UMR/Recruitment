830501c7b6011a7bfc2b746a243b9728fb3007d4
Sohel Rana <srana@ael-bd.com>
2023-05-31 13:54:09 +0600

    constant added

--- Recruitment.Web/src/app/common/constant.js
+++ Recruitment.Web/src/app/common/constant.js
@@ -1,29 +1,25 @@
-﻿export const authCookieKey = 'tokenKeyUMRRecruitment';
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.resourceServerUrl = exports.scopes = exports.pathMatch = exports.authorizationServerUrl = exports.authCookieKey = void 0;
+exports.authCookieKey = 'tokenKeyUMRRecruitment';
 //////////////////////////////   Localhost   ///////////////////////////
-export const authorizationServerUrl = 'http://localhost:4000';
-export const pathMatch = '/RecruitmentSPA';
-export const scopes = ['recruitment.fullaccess', 'openid'];
-export const resourceServerUrl = 'http://localhost:4001';
-
+exports.authorizationServerUrl = 'http://localhost:4000';
+exports.pathMatch = '/RecruitmentSPA';
+exports.scopes = ['recruitment.fullaccess', 'openid'];
+exports.resourceServerUrl = 'http://localhost:4001';
 //////////////////////    172.16.1.11 TEST   //////////////////////////
 //export const authorizationServerUrl = 'http://www.umrtest.com/UMRRecruitmentBasicAPIAuthorizationServer';
 //export const pathMatch = '/RecruitmentSPA';
 //export const scopes = ['recruitment.fullaccess', 'offline_access', 'openid'];
 //export const resourceServerUrl = 'http://www.umrtest.com/UMRRecruitmentBasicAPIResourceServer';
-
 //////////////////////////    172.16.1.104 PRODUCTION  //////////////////////////
 //export const authorizationServerUrl = 'https://www.universalmedicalrecord.com/UMRRecruitmentBasicAPIAuthorizationServer';
 //export const pathMatch = '/RecruitmentSPA';
 //export const scopes = ['recruitment.fullaccess', 'offline_access', 'openid'];
 //export const resourceServerUrl = 'https://www.universalmedicalrecord.com/UMRRecruitmentBasicAPIResourceServer';
-
 //////////////////////////   mechatronicssolutionscorp  //////////////////////////
 //export const authorizationServerUrl = 'https://www.mechatronicssolutionscorp.com/UMRRecruitmentBasicAPIAuthorizationServer';
 //export const pathMatch = '/RecruitmentSPA';
 //export const scopes = ['recruitment.fullaccess', 'offline_access', 'openid'];
 //export const resourceServerUrl = 'https://www.mechatronicssolutionscorp.com/UMRRecruitmentBasicAPIResourceServer';
-
-
-
-
+//# sourceMappingURL=constant.js.map
