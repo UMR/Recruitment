e62aa86861e2de98a3ebaeb58c6a1fb1c2462911
Sohel Rana <srana@ael-bd.com>
2023-06-01 20:37:15 +0600

    Update agency.service.ts

--- Recruitment.Web/src/app/pages/information/agency/agency.service.ts
+++ Recruitment.Web/src/app/pages/information/agency/agency.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AgencyService {
 
-  constructor() { }
+    constructor(private http: HttpClient) { }
+
+    getAllAgency(): Observable<any> {
+        return this.http.get()
+
+    }
 }
