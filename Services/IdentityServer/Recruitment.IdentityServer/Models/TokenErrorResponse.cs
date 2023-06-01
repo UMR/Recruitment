0727ca44f09030c338d3c9785775c17aaf8d53dd
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-06-01 16:30:46 +0600

    add model

---
+++ Services/IdentityServer/Recruitment.IdentityServer/Models/TokenErrorResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace Recruitment.IdentityServer.Models
+{
+    public class TokenErrorResponse
+    {
+        [JsonProperty("errorDescription")]
+        public string ErrorDescription { get; set; }
+
+        [JsonProperty("isError")]
+        public bool IsError { get; set; }                
+
+        [JsonProperty("httpErrorReason")]
+        public string HttpErrorReason { get; set; }
+
+        [JsonProperty("error")]
+        public string Error { get; set; }
+    }
+}
