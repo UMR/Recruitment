8077ee6d32a7cfaf100bc74cd964b7b2ef202758
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-06-08 18:01:41 +0600

    email type mapping

--- Services/Recruitment/Recruitment.Application/Mappings/MappingProfile.cs
+++ Services/Recruitment/Recruitment.Application/Mappings/MappingProfile.cs
@@ -5,6 +5,7 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         CreateMap<Menu, MenuListDto>().ReverseMap();
-        CreateMap<Agency, AgencyListDto>().ReverseMap();       
+        CreateMap<Agency, AgencyListDto>().ReverseMap();
+        CreateMap<EmailType, EmailTypeListDto>().ReverseMap();
     }
 }
