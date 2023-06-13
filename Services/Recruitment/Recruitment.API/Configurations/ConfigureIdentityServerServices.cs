650da84f82793a95fa90d61d941f5c3972bff67b
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-06-13 16:30:29 +0600

    add valid issuer in appsettings

--- Services/Recruitment/Recruitment.API/Configurations/ConfigureIdentityServerServices.cs
+++ Services/Recruitment/Recruitment.API/Configurations/ConfigureIdentityServerServices.cs
@@ -1,45 +1,20 @@
-﻿using System.Security.Cryptography.X509Certificates;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.IdentityModel.Tokens;
-
-namespace Recruitment.API.Configurations;
+﻿namespace Recruitment.API.Configurations;
 
 public static class IdentityServerServicesRegistration
 {
     public static WebApplicationBuilder ConfigureIdentityServerServices(this WebApplicationBuilder builder)
     {
-        //builder.Services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
-        //      .AddIdentityServerAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme, options => {
-        //          options.Authority = builder.Configuration["IdentityServer:Authority"];
-        //          options.RequireHttpsMetadata = false;
-        //          options.ApiName = builder.Configuration["IdentityServer:ApiName"];
-        //          options.TokenValidationParameters.ValidIssuers = new[]
-        //            {
-        //                "http://localhost:5000",
-        //                "http://127.0.0.1:5000",
-        //            };
-        //      });
-
-
         builder.Services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
             .AddIdentityServerAuthentication(
                 IdentityServerAuthenticationDefaults.AuthenticationScheme,
                 options => {
                     options.Authority = builder.Configuration["IdentityServer:Authority"];
-                    options.RequireHttpsMetadata = false;
-
-                    // This previously was: options.ApiName = scopeName;
-                    options.Audience = builder.Configuration["IdentityServer:ApiName"]; 
-
-                    // Option 1: if you want to turn off issuer validation
-                    options.TokenValidationParameters.ValidateIssuer = false;
-
-                    // Option 2: if you want to support multiple issuers
+                    options.RequireHttpsMetadata = false;                    
+                    options.Audience = builder.Configuration["IdentityServer:ApiName"];                    
+                    options.TokenValidationParameters.ValidateIssuer = false;                    
                     //options.TokenValidationParameters.ValidIssuers = new[]
                     //{
-                    //    "http://localhost:5000",
-                    //    "http://127.0.0.1:5000",
+                    //    builder.Configuration["IdentityServer:ValidIssuer"]
                     //};
                 },
                 null
