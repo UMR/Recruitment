72b2fecd9110f131bda944c4029e333adfd84bad
Sohel Rana <srana@ael-bd.com>
2023-06-16 17:09:49 +0600

    institution

---
+++ Services/Recruitment/Recruitment.Application/Contracts/Persistence/IInstitutionRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Recruitment.Application.Contracts.Persistence;
+
+public interface IInstitutionRepository
+{
+    Task <bool> GetInstitutionByInstituteTypeIdAsync(int id);
+    
+}
---
+++ Services/Recruitment/Recruitment.Persistence/Repositories/InstitutionRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace Recruitment.Persistence.Repositories;
+
+public class InstitutionRepository : IInstitutionRepository
+{
+    private readonly IDapperContext _dapperContext;
+
+    public InstitutionRepository(IDapperContext dapperContext)
+    {
+        _dapperContext = dapperContext;
+    }
+
+    public async Task<bool> GetInstitutionByInstituteTypeIdAsync(int id)
+    {
+        var query = "UPDATE [InstituteType]  SET [InstituteType] = @InstituteType,[Description] = @Description ," +
+             "[UpdatedBy] = @UpdatedBy ,[UpdatedDate] = @UpdatedDate WHERE InstituteTypeID = @InstituteTypeID";
+
+        var parameters = new DynamicParameters();
+        parameters.Add("InstituteTypeID", id, DbType.Int32);
+
+        using (IDbConnection conn = _dapperContext.CreateConnection)
+        {
+            var result = await conn.ExecuteAsync(query, parameters);
+            return result > 0 ? true : false;
+        }
+    }
+}
