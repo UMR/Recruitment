0ca4f3df4532417d02fca94b94f62b99bb68f048
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-06-20 15:57:41 +0600

    write all repository methods for position license requirement

--- Services/Recruitment/Recruitment.Application/Contracts/Persistence/IPositionLicenseRequirementRepository.cs
+++ Services/Recruitment/Recruitment.Application/Contracts/Persistence/IPositionLicenseRequirementRepository.cs
@@ -2,5 +2,16 @@
 {
     public interface IPositionLicenseRequirementRepository
     {
+        Task<IEnumerable<PositionLicenseRequirement>> GetAllAsync();
+
+        Task<PositionLicenseRequirement> GetByIdAsync(long id);
+
+        Task<bool> IsExistAsync(string name, int? id = null);
+
+        Task<int> CreateAsync(PositionLicenseRequirement model);
+
+        Task<bool> UpdateAsync(long id, PositionLicenseRequirement model);
+
+        Task<bool> DeleteAsync(long id);
     }
 }
--- Services/Recruitment/Recruitment.Persistence/Repositories/PositionLicenseRequirementRepository.cs
+++ Services/Recruitment/Recruitment.Persistence/Repositories/PositionLicenseRequirementRepository.cs
@@ -1,6 +1,110 @@
 ﻿namespace Recruitment.Persistence.Repositories
 {
-    public class PositionLicenseRequirementRepository:IPositionLicenseRequirementRepository
+    public class PositionLicenseRequirementRepository : IPositionLicenseRequirementRepository
     {
+        private readonly IDapperContext _dapperContext;
+
+        public PositionLicenseRequirementRepository(IDapperContext dapperContext)
+        {
+            _dapperContext = dapperContext;
+        }
+
+        public async Task<IEnumerable<PositionLicenseRequirement>> GetAllAsync()
+        {
+            var query = @"SELECT * FROM PositionLicenseRequirement ORDER BY PositionLicenseRequirementName ASC";
+
+            using (IDbConnection conn = _dapperContext.CreateConnection)
+            {
+                var result = await conn.QueryAsync<PositionLicenseRequirement>(query);
+                return result.ToList();
+            }
+        }
+
+        public async Task<PositionLicenseRequirement> GetByIdAsync(long id)
+        {
+            var query = @"SELECT * FROM PositionLicenseRequirement WHERE PositionLicenseRequirementID=@PositionLicenseRequirementID";
+
+            var parameters = new DynamicParameters();
+            parameters.Add("PositionLicenseRequirementID", id, DbType.Int64);
+
+            using (IDbConnection conn = _dapperContext.CreateConnection)
+            {
+                var result = await conn.QueryFirstOrDefaultAsync<PositionLicenseRequirement>(query, parameters);
+                return result;
+            }
+        }
+
+        public async Task<bool> IsExistAsync(string name, int? id = null)
+        {
+            var query = @"SELECT COUNT(*) FROM PositionLicenseRequirement WHERE PositionLicenseRequirementName=@PositionLicenseRequirementName";
+
+            if (id != null)
+            {
+                query += " AND PositionLicenseRequirementID != @PositionLicenseRequirementID";                
+            }
+
+            var parameters = new DynamicParameters();
+            parameters.Add("PositionLicenseRequirementName", name, DbType.String);
+
+            if (id is not null)
+            {
+                parameters.Add("PositionLicenseRequirementID", id, DbType.Int64);
+            }
+
+            using (IDbConnection conn = _dapperContext.CreateConnection)
+            {
+                var result = await conn.ExecuteScalarAsync<int>(query, parameters);
+                return result > 0 ? true : false;
+            }
+        }
+
+        public async Task<int> CreateAsync(PositionLicenseRequirement model)
+        {
+            var query = "INSERT INTO PositionLicenseRequirement (PositionLicenseRequirementName, CreatedBy, CreatedDate) VALUES (@PositionLicenseRequirementName, @CreatedBy, @CreatedDate) " +
+                        "SELECT CAST(SCOPE_IDENTITY() as int)";
+
+            var parameters = new DynamicParameters();
+            parameters.Add("PositionLicenseRequirementName", model.PositionLicenseRequirementName, DbType.String);            
+            parameters.Add("CreatedBy", model.CreatedBy, DbType.Int32);
+            parameters.Add("CreatedDate", model.CreatedDate, DbType.DateTime);
+
+            using (IDbConnection conn = _dapperContext.CreateConnection)
+            {
+                var id = await conn.ExecuteAsync(query, parameters);
+                return id;
+            }
+        }
+
+        public async Task<bool> UpdateAsync(long id, PositionLicenseRequirement model)
+        {
+            var query = "UPDATE PositionLicenseRequirement SET PositionLicenseRequirementName = @PositionLicenseRequirementName, UpdatedBy = @UpdatedBy, UpdatedDate = @UpdatedDate " +
+                "WHERE PositionLicenseRequirementID = @PositionLicenseRequirementID";
+
+            var parameters = new DynamicParameters();
+            parameters.Add("PositionLicenseRequirementName", model.PositionLicenseRequirementName, DbType.String);
+            parameters.Add("UpdatedBy", model.UpdatedBy, DbType.Int32);
+            parameters.Add("UpdatedDate", model.UpdatedDate, DbType.DateTime);
+            parameters.Add("PositionLicenseRequirementID", id, DbType.Int64);
+
+            using (IDbConnection conn = _dapperContext.CreateConnection)
+            {
+                var result = await conn.ExecuteAsync(query, parameters);
+                return result > 0 ? true : false;
+            }
+        }
+
+        public async Task<bool> DeleteAsync(long id)
+        {
+            var query = "DELETE FROM PositionLicenseRequirement WHERE PositionLicenseRequirementID = @PositionLicenseRequirementID";
+
+            var parameters = new DynamicParameters();
+            parameters.Add("PositionLicenseRequirementID", id, DbType.Int64);
+
+            using (IDbConnection conn = _dapperContext.CreateConnection)
+            {
+                var result = await conn.ExecuteAsync(query, parameters);
+                return result > 0 ? true : false;
+            }
+        }        
     }
 }
