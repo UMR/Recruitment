0b58e80129458f32cc9b35e66e6057811f7e68cd
Sohel Rana <srana@ael-bd.com>
2023-06-20 14:37:36 +0600

    user model added

--- Recruitment.Web/src/app/common/models/user.model.ts
+++ Recruitment.Web/src/app/common/models/user.model.ts
@@ -1,17 +1,17 @@
 ﻿export interface User {
     userId: number;
-    LoginId: string;
-    FirstName: string;
-    LastName: string;
-    Password: string;
-    Email: string;
-    Telephone: string;
-    Odapermission: boolean;
-    IsActive: boolean;
-    CreatedBy: number;
-    CreatedDate: Date;
-    UpdatedBy: number;
-    UpdatedDate: Date;
-    TimeOut: number;
-    AgencyId: number;
+    loginId: string;
+    firstName: string;
+    lastName: string;
+    password: string;
+    email: string;
+    telephone: string;
+    odapermission: boolean;
+    isActive: boolean;
+    createdBy: number;
+    createdDate: Date;
+    updatedBy: number;
+    updatedDate: Date;
+    timeOut: number;
+    agencyId: number;
 }
