0ce7b41fb94da3df7e1a3a2917f08ad4f7bc09c6
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-06-21 18:15:50 +0600

    add angular service for special word

--- Recruitment.Web/src/app/pages/information/special-word/special-word.module.ts
+++ Recruitment.Web/src/app/pages/information/special-word/special-word.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { SpecialWordRoutingModule } from './special-word-routing.module';
 import { SpecialWordComponent } from './special-word.component';
+import { SpecialWordService } from '../../../common/service/special-word.service';
+
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DialogModule } from 'primeng/dialog';
+import { ConfirmationService, MessageService } from 'primeng/api';
 
 
 @NgModule({
     declarations: [SpecialWordComponent],
-  imports: [
-    CommonModule,
-    SpecialWordRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        SpecialWordRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        TableModule,
+        ToastModule,
+        ConfirmDialogModule,
+        DialogModule
+    ],
+    providers: [ConfirmationService, MessageService, SpecialWordService]
 })
 export class SpecialWordModule { }
