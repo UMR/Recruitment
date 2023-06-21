24ad83b640fac9c1c1789daffb32bf23e6cf697a
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-06-21 19:45:27 +0600

    add upper case module

--- Recruitment.Web/src/app/pages/information/upper-case-word/upper-case-word.module.ts
+++ Recruitment.Web/src/app/pages/information/upper-case-word/upper-case-word.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UpperCaseWordRoutingModule } from './upper-case-word-routing.module';
+import { UpperCaseWordComponent } from './upper-case-word.component';
 
 
 @NgModule({
-  declarations: [],
+    declarations: [UpperCaseWordComponent],
   imports: [
     CommonModule,
     UpperCaseWordRoutingModule
