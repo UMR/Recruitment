7ea25c947970cfa508b49f781eceab7747c8ca2e
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-06-22 14:22:09 +0600

    add service and model for lower

---
+++ Recruitment.Web/src/app/common/models/lower-case-word.model.ts
@@ -0,0 +1,4 @@
+﻿export interface LowerCaseWordModel {
+    id: number;
+    word: string;    
+}
--- Recruitment.Web/src/app/pages/information/lower-case-word/lower-case-word.module.ts
+++ Recruitment.Web/src/app/pages/information/lower-case-word/lower-case-word.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LowerCaseWordRoutingModule } from './lower-case-word-routing.module';
 import { LowerCaseWordComponent } from './lower-case-word.component';
 
 
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DialogModule } from 'primeng/dialog';
+import { ConfirmationService, MessageService } from 'primeng/api';
+
+
 @NgModule({
     declarations: [LowerCaseWordComponent],
-  imports: [
-    CommonModule,
-    LowerCaseWordRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        LowerCaseWordRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        TableModule,
+        ToastModule,
+        ConfirmDialogModule,
+        DialogModule
+    ],
+    providers: [ConfirmationService, MessageService ]
 })
 export class LowerCaseWordModule { }
