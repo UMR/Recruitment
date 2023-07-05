bf83b0b6de9a4874ac32eab123559a9c9c34a34f
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-07-05 18:23:47 +0600

    add auto focus  onenter trim in lower case

--- Recruitment.Web/src/app/pages/information/lower-case-word/lower-case-word.component.ts
+++ Recruitment.Web/src/app/pages/information/lower-case-word/lower-case-word.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ConfirmationService, MessageService } from 'primeng/api';
 
@@ -15,9 +15,12 @@ export class LowerCaseWordComponent {
     public lowerCaseWords: LowerCaseWordModel[] = [];
     public formGroup!: FormGroup;
     public visibleDialog: boolean = false;
+    public isSubmitted: boolean = false;
+    public addEditTitle: string | undefined;
 
-    constructor(private formBuilder: FormBuilder, private messageService: MessageService, private confirmationService: ConfirmationService,
-        private lowerCaseWordService: LowerCaseWordService) { }
+    constructor(private formBuilder: FormBuilder, private messageService: MessageService,
+        private confirmationService: ConfirmationService, private renderer: Renderer2,
+        private el: ElementRef, private lowerCaseWordService: LowerCaseWordService) { }
 
     ngOnInit(): void {
         this.createFormGroup();
@@ -35,21 +38,80 @@ export class LowerCaseWordComponent {
     }
 
     onAdd(): void {
+        this.addEditTitle = 'Add';
         this.id = 0;
-        this.formGroup.reset();
-        this.visibleDialog = true;
+        this.clearFields(false, true);
     }
 
     onEdit(model: LowerCaseWordModel) {
+        this.addEditTitle = 'Edit';
         this.id = model.id;
         this.formGroup.patchValue({
             word: model.word,
         });
         this.visibleDialog = true;
+    }       
+
+    onClear() {
+        this.clearFields(false, true);
+        this.renderer.selectRootElement('#word').focus();
     }
 
     onCancel() {
-        this.visibleDialog = false;
+        this.clearFields(false, false);
+    } 
+
+    onSave(): void {
+        this.isSubmitted = true;
+        const model: any = {
+            id: this.id,
+            word: this.formGroup.controls['word'].value ? this.formGroup.controls['word'].value.trim() : null,
+        };
+        if (this.formGroup.valid) {
+            if (this.id === 0) {
+                this.lowerCaseWordService.create(model).subscribe({
+                    next: (res) => {
+                        if (res.status === 200) {
+                            if ((res.body as any).success) {
+                                this.clearFields(false, false);
+                                this.getLowerCaseWords();
+                                this.messageService.add({ severity: 'success', summary: 'Successful', detail: (res.body as any).message, life: 3000 });
+                            } else {
+                                this.messageService.add({ severity: 'error', summary: 'Error', detail: (res.body as any).errors[0], life: 3000 });
+                            }
+                        }
+                    },
+                    error: (err) => {
+                        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Creating Failed', life: 3000 });
+                    }
+                });
+            } else {
+                this.lowerCaseWordService.update(this.id, model).subscribe({
+                    next: (res) => {
+                        if (res.status === 200) {
+                            if ((res.body as any).success) {
+                                this.clearFields(false, false);                                
+                                this.getLowerCaseWords();
+                                this.messageService.add({ severity: 'success', summary: 'Successful', detail: (res.body as any).message, life: 3000 });
+                            } else {
+                                this.messageService.add({ severity: 'error', summary: 'Error', detail: (res.body as any).errors[0], life: 3000 });
+                            }
+                        }
+                    },
+                    error: (err) => {
+                        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Updating Failed', life: 3000 });
+                    }
+                });
+            }
+        } else {
+            for (const key of Object.keys(this.formGroup.controls)) {
+                if (this.formGroup.controls[key].invalid) {
+                    const invalidControl = this.el.nativeElement.querySelector('[formcontrolname="' + key + '"]');
+                    invalidControl.focus();
+                    break;
+                }
+            }
+        }
     }
 
     onDelete(model: LowerCaseWordModel) {
@@ -62,6 +124,7 @@ export class LowerCaseWordComponent {
                     next: (res) => {
                         if (res.status === 200) {
                             if ((res.body as any).success) {
+                                this.clearFields(false, false);
                                 this.getLowerCaseWords();
                                 this.messageService.add({ severity: 'success', summary: 'Successful', detail: (res.body as any).message, life: 3000 });
                             } else {
@@ -79,55 +142,10 @@ export class LowerCaseWordComponent {
         });
     }
 
-    onClear() {
+    clearFields(isSubmitted: boolean, visibleDialog: boolean) {
+        this.isSubmitted = isSubmitted;
+        this.visibleDialog = visibleDialog;
         this.formGroup.reset();
-    }
-
-    onSave(): void {
-
-        const model: any = {
-            id: this.id,
-            word: this.formGroup.controls['word'].value ? this.formGroup.controls['word'].value.trim() : null,
-        };
-
-        if (this.formGroup.valid) {
-            if (this.id === 0) {
-                this.lowerCaseWordService.create(model).subscribe({
-                    next: (res) => {
-                        if (res.status === 200) {
-                            if ((res.body as any).success) {
-                                this.visibleDialog = false;
-                                this.getLowerCaseWords();
-                                this.messageService.add({ severity: 'success', summary: 'Successful', detail: (res.body as any).message, life: 3000 });
-                            } else {
-                                this.messageService.add({ severity: 'error', summary: 'Error', detail: (res.body as any).errors[0], life: 3000 });
-                            }
-                        }
-                    },
-                    error: (err) => {
-                        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Creating Failed', life: 3000 });
-                    }
-                });
-            } else {
-                this.lowerCaseWordService.update(this.id, model).subscribe({
-                    next: (res) => {
-                        if (res.status === 200) {
-                            if ((res.body as any).success) {
-                                this.id = 0;
-                                this.visibleDialog = false;
-                                this.getLowerCaseWords();
-                                this.messageService.add({ severity: 'success', summary: 'Successful', detail: (res.body as any).message, life: 3000 });
-                            } else {
-                                this.messageService.add({ severity: 'error', summary: 'Error', detail: (res.body as any).errors[0], life: 3000 });
-                            }
-                        }
-                    },
-                    error: (err) => {
-                        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Updating Failed', life: 3000 });
-                    }
-                });
-            }
-        }
     }
 
     getLowerCaseWords() {
--- Recruitment.Web/src/app/pages/information/lower-case-word/lower-case-word.module.ts
+++ Recruitment.Web/src/app/pages/information/lower-case-word/lower-case-word.module.ts
@@ -5,6 +5,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LowerCaseWordRoutingModule } from './lower-case-word-routing.module';
 import { LowerCaseWordComponent } from './lower-case-word.component';
 import { LowerCaseWordService } from '../../../common/service/lower-case-word.service';
+import { CommonDirectiveModule } from '../../../common/directives/common-directive.module';
 
 import { TableModule } from 'primeng/table';
 import { ToastModule } from 'primeng/toast';
@@ -23,7 +24,8 @@ import { ConfirmationService, MessageService } from 'primeng/api';
         TableModule,
         ToastModule,
         ConfirmDialogModule,
-        DialogModule
+        DialogModule,
+        CommonDirectiveModule
     ],
     providers: [ConfirmationService, MessageService, LowerCaseWordService]
 })
