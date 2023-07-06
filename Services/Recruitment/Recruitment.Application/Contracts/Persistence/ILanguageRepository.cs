9ab7a1bdc14854a115912bb9251ee982997b8452
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-07-06 15:43:07 +0600

    implement language repository

--- Services/Recruitment/Recruitment.Application/Contracts/Persistence/ILanguageRepository.cs
+++ Services/Recruitment/Recruitment.Application/Contracts/Persistence/ILanguageRepository.cs
@@ -2,4 +2,16 @@
 
 public interface ILanguageRepository
 {
+    Task<IEnumerable<Language>> GetAllAsync();
+
+    Task<Language> GetByIdAsync(int id);
+
+    Task<bool> IsExistVisaTypeAsync(string visaType, int? id = null);
+
+    Task<int> CreateAsync(Language model);
+
+    Task<bool> UpdateAsync(int id, Language model);
+
+    Task<bool> DeleteAsync(int id);
+    
 }
--- Services/Recruitment/Recruitment.Persistence/Repositories/LanguageRepository.cs
+++ Services/Recruitment/Recruitment.Persistence/Repositories/LanguageRepository.cs
@@ -1,12 +1,104 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Recruitment.Persistence.Repositories;
 
-namespace Recruitment.Persistence.Repositories
+public class LanguageRepository : ILanguageRepository
 {
-    internal class LanguageRepository
+    private readonly IDapperContext _dapperContext;
+
+    public LanguageRepository(IDapperContext dapperContext)
     {
+        _dapperContext = dapperContext;
+    }
+
+    public async Task<IEnumerable<Language>> GetAllAsync()
+    {
+        var query = @"SELECT * FROM Language ORDER BY Name ASC";
+
+        using (IDbConnection conn = _dapperContext.CreateConnection)
+        {
+            var result = await conn.QueryAsync<Language>(query);
+            return result.ToList();
+        }
+    }
+
+    public async Task<Language> GetByIdAsync(int id)
+    {
+        var query = @"SELECT * FROM Language WHERE LanguageID=@LanguageID";
+
+        var parameters = new DynamicParameters();
+        parameters.Add("LanguageID", id, DbType.Int32);
+
+        using (IDbConnection conn = _dapperContext.CreateConnection)
+        {
+            var result = await conn.QueryFirstOrDefaultAsync<Language>(query, parameters);
+            return result;
+        }
+    }
+
+    public async Task<bool> IsExistVisaTypeAsync(string visaType, int? id = null)
+    {
+        var query = @"SELECT COUNT(*) FROM Language WHERE Name=@Name";
+
+        if (id != null)
+        {
+            query += " AND LanguageID != @LanguageID";
+        }
+
+        var parameters = new DynamicParameters();
+        parameters.Add("Name", visaType, DbType.String);
+
+        if (id is not null)
+        {
+            parameters.Add("LanguageID", id, DbType.Int32);
+        }
+
+        using (IDbConnection conn = _dapperContext.CreateConnection)
+        {
+            var result = await conn.ExecuteScalarAsync<int>(query, parameters);
+            return result > 0 ? true : false;
+        }
+    }
+
+    public async Task<int> CreateAsync(Language model)
+    {
+        var query = "INSERT INTO Language (Name) VALUES (@Name) " +
+                    "SELECT CAST(SCOPE_IDENTITY() as int)";
+
+        var parameters = new DynamicParameters();
+        parameters.Add("Name", model.Name, DbType.String);
+
+        using (IDbConnection conn = _dapperContext.CreateConnection)
+        {
+            var id = await conn.ExecuteAsync(query, parameters);
+            return id;
+        }
+    }
+
+    public async Task<bool> UpdateAsync(int id, Language model)
+    {
+        var query = "UPDATE Language SET Name = @Name WHERE LanguageID = @LanguageID";
+
+        var parameters = new DynamicParameters();
+        parameters.Add("Name", model.Name, DbType.String);
+        parameters.Add("LanguageID", id, DbType.Int32);
+
+        using (IDbConnection conn = _dapperContext.CreateConnection)
+        {
+            var result = await conn.ExecuteAsync(query, parameters);
+            return result > 0 ? true : false;
+        }
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var query = "DELETE FROM Language WHERE LanguageID = @LanguageID";
+
+        var parameters = new DynamicParameters();
+        parameters.Add("LanguageID", id, DbType.Int32);
+
+        using (IDbConnection conn = _dapperContext.CreateConnection)
+        {
+            var result = await conn.ExecuteAsync(query, parameters);
+            return result > 0 ? true : false;
+        }
     }
 }
