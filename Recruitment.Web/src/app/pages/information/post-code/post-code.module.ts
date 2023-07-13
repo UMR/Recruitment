a21f750084c3d2a9bd873bd9b0ee2781b9bb4033
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-07-13 20:32:10 +0600

    add and implement post code service

--- Recruitment.Web/src/app/pages/information/post-code/post-code.module.ts
+++ Recruitment.Web/src/app/pages/information/post-code/post-code.module.ts
@@ -1,15 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { PostCodeRoutingModule } from './post-code-routing.module';
 import { PostCodeComponent } from './post-code.component';
+import { PostCodeService } from '../../../common/service/post-code.service';
+import { CommonDirectiveModule } from '../../../common/directives/common-directive.module';
+
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DialogModule } from 'primeng/dialog';
+import { ConfirmationService, MessageService } from 'primeng/api';
 
 
 @NgModule({
     declarations: [PostCodeComponent],
-  imports: [
-    CommonModule,
-    PostCodeRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        PostCodeRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        TableModule,
+        ToastModule,
+        ConfirmDialogModule,
+        DialogModule,
+        CommonDirectiveModule
+    ],
+    providers: [ConfirmationService, MessageService, PostCodeService]
 })
 export class PostCodeModule { }
